3b770e9340ae4a499149d609925bfa7649aeb50f
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-10-27 00:18:15 +0900

    E302 expected 2 blank lines, found
    0
    ./bot.py:15:1: E302 expected 2 blank lines, found 1
    ./bot.py:20:1: E302 expected 2 blank lines, found 0
    ./bot.py:30:9: F841 local variable 'e' is assigned to but never used
    ./bot.py:34:1: E302 expected 2 blank lines, found 1
    ./bot.py:38:1: E305 expected 2 blank lines after class or function definition, found 1
    3     E302 expected 2 blank lines, found 1
    1     E305 expected 2 blank lines after class or function definition, found 1
    1     F841 local variable 'e' is assigned to but never used
    5

--- build_container_bot/bot.py
+++ build_container_bot/bot.py
@@ -12,11 +12,14 @@ ADDRESS = os.getenv('minecraft_server_config_rcon_host', 'localhost')
 PASSWORD = os.getenv('minecraft_server_config_rcon_password', '')
 PORT = os.getenv('minecraft_server_config_rcon_port', 25575)
 
+
 def loop(interval=0.1):
     # @args interval: [float] unit: second
     while True:
         rcon(ADDRESS, PASSWORD, PORT)
         time.sleep(interval)
+
+
 def rcon(address, password, port):
     addrs = socket.getaddrinfo(ADDRESS, 'a')
     for addr in addrs:
@@ -31,9 +34,11 @@ def rcon(address, password, port):
             traceback.format_exc()
             sys.exit(1)
 
+
 def test_main():
     # pytest
     assert True
+
 
 if __name__ == '__main__':
     loop(10)
