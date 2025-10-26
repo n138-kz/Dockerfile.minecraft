b76d7674f710a32f9a6f4793fd603943acbbb6a9
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-10-26 23:54:33 +0900

    Update bot.py

--- build_container_bot/bot.py
+++ build_container_bot/bot.py
@@ -33,8 +33,8 @@ def rcon(address, password, port):
             sys.exit(1)
 
 def test_main():
-  # pytest
-  assert True
+    # pytest
+    assert True
 
 if __name__ == '__main__':
-  loop(10)
+    loop(10)
