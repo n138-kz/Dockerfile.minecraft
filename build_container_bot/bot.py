96a56ca1805515578939917eaddd9b6beef857b7
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-10-26 23:08:10 +0900

    def loop

--- build_container_bot/bot.py
+++ build_container_bot/bot.py
@@ -2,20 +2,25 @@
 # https://pypi.org/project/mcrcon/
 from mcrcon import MCRcon
 import os
+import time
 
 # アドレス、パスワード、ポートの定義
 ADDRESS = os.getenv('minecraft_server_config_rcon_host', 'localhost')
 PASSWORD = os.getenv('minecraft_server_config_rcon_password', '')
 PORT = os.getenv('minecraft_server_config_rcon_port', 25575)
 
-def main(address, password, port):
+def loop(interval=0.1):
+  # @args interval: [float] unit: second
+  rcon(ADDRESS, PASSWORD, PORT)
+def rcon(address, password, port):
   with MCRcon(address, password, port) as rcon:
     # コマンドを送信する
-    rcon.command("list")
+    result = rcon.command("list")
+    print(result)
 
 def test_main():
   # pytest
   assert True
 
 if __name__ == '__main__':
-  main(ADDRESS, PASSWORD, PORT)
+  loop(10)
