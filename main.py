6b7e0803b3a28ccaa1b50df0491e6b0e001f8d00
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-11-11 00:54:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -282,10 +282,11 @@ async def ping(ctx: discord.Interaction):
 @tree.command(name="list", description="現在サーバーに接続しているプレイヤーのリストを表示")
 async def mcrcon_list(ctx: discord.Interaction):
     try:
+        result = 'There are 0 of a max of 10 players online:'
         title = '[mcrcon] Result: /list'
         description = ''
         description += '```\n'
-        description += '/list'
+        description += result
         description += '\n'
         description += '```\n'
         color = template['color']['success']
