5a594d63d905ba92ec3b9636f97ebc49c605655b
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-12-08 23:44:49 +0900

    Update mods-pages-build.py

--- docs/mods-pages-build.py
+++ docs/mods-pages-build.py
@@ -6,3 +6,30 @@ with open(datafile, mode='r') as f:
     payload=json.load(f)
 
 print(payload['mods'])
+result=''
+result+=''
+result+='# applied mods / datapacks\n'
+result+='\n'
+result+='## mods\n'
+result+='\n'
+result+='| Icon | Name | Descr |\n'
+result+='| ---- | ---- | ----- |\n'
+for line in payload['mods']:
+    result+='| <img src="{}" style="height: 3em;" /> | {} | {} |\n'.format(
+        line['icon'],
+        line['name'],
+        line['description'],
+    )
+result+='\n'
+result+='## datapacks\n'
+result+='\n'
+result+='| Name | Descr |\n'
+result+='| ---- | ----- |\n'
+for line in payload['datapack']:
+    result+='| {} | {} |\n'.format(
+        line['name'],
+        line['description'],
+    )
+result+='\n'
+result+=''
+print(result)
